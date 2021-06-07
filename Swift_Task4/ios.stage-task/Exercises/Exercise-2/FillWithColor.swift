001f238f09fd542337f9f4ab275d8d036ae07597
skuratovm <maydibee@gmail.com>
2021-06-07 23:20:57 +0300

    feat:add ex 2 & ex 3

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -1,8 +1,62 @@
 import Foundation
 
-final class FillWithColor {
-    
-    func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
-    }
+struct Element: Equatable {
+    var row: Int
+    var column: Int
 }
+
+
+final class FillWithColor {
+    func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
+    
+        if image.isEmpty || row < 0 || column < 0 || row >= image.count || column >= image[0].count {
+                    return image
+                }
+        var outImage = image
+                var queue = [Element(row: row, column: column)]
+                var proceedElements = Array<Element>()
+                let elementColor = image[row][column]
+        
+        while queue.count > 0 {
+                    let element = queue.remove(at: 0)
+                    proceedElements.append(element)
+                    
+                    if (image[element.row][element.column] == elementColor) {
+                        outImage[element.row][element.column] = newColor
+                    } else {
+                        continue
+                    }
+                    
+                    if (element.row - 1) >= 0 {
+                        let element = Element(row: element.row - 1, column: element.column)
+                        if !proceedElements.contains(element) {
+                            queue.append(element)
+                        }
+                    }
+                    if (element.row + 1) < image.count {
+                        let element = Element(row: element.row + 1, column: element.column)
+                        if !proceedElements.contains(element) {
+                            queue.append(element)
+                        }
+                    }
+                    if (element.column - 1) >= 0 {
+                        let element = Element(row: element.row, column: element.column - 1)
+                        if !proceedElements.contains(element) {
+                            queue.append(element)
+                        }
+                    }
+                    if (element.column + 1) < image[row].count {
+                        let element = Element(row: element.row, column: element.column + 1)
+                        if !proceedElements.contains(element) {
+                            queue.append(element)
+                        }
+                    }
+                }
+                return outImage
+        
+    
+    
+    }
+    
+}
+
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,37 +1,131 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var usersStorage: Set <User> = []
+    var callsStorage: [CallID : Call] = [:]
+    var currentCallsStorage: [UUID : Call] = [:]
+    
+}
+extension User: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
 
 extension CallStation: Station {
     func users() -> [User] {
-        []
+       Array(usersStorage)
     }
     
     func add(user: User) {
-
+        usersStorage.insert(user)
     }
     
     func remove(user: User) {
-
+        usersStorage.remove(user)
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        switch action {
+                
+                case let .start(from: from, to: to):
+                    
+                    let callID = CallID()
+                    if !usersStorage.contains(from){
+                        return nil
+                    }
+                    if !usersStorage.contains(to){
+                        let call = Call(id: callID, incomingUser: from, outgoingUser: to, status: .ended(reason: .error))
+                        callsStorage[callID] = call
+                        currentCallsStorage[from.id] = nil
+                        currentCallsStorage[to.id] = nil
+                        return callID
+                    }
+                    if currentCall(user: to) != nil{
+                        let call = Call(id: callID, incomingUser: from, outgoingUser: to, status: .ended(reason: .userBusy))
+                        callsStorage[callID] = call
+                        return callID
+                    }
+                    let call = Call(id: callID, incomingUser: from, outgoingUser: to, status: .calling)
+                    callsStorage[callID] = call
+                    currentCallsStorage[from.id] = call
+                    currentCallsStorage[to.id] = call
+                    return callID
+                
+                case .answer(from: let to):
+                    guard let call = currentCall(user: to) else {
+                        return nil
+                    }
+                    
+                    let from = call.incomingUser
+                    if !usersStorage.contains(to) || !usersStorage.contains(from){
+                        let badCall = Call(id: call.id, incomingUser: from, outgoingUser: to, status: .ended(reason: .error))
+                        callsStorage[badCall.id] = badCall
+                        currentCallsStorage[to.id] = nil
+                        currentCallsStorage[from.id] = nil
+                        return nil
+                    }
+                    
+                    if let toCall = currentCall(user: to) {
+                        if (toCall.status != .calling || toCall.id != call.id) {
+                            let badCall = Call(id: call.id, incomingUser: from, outgoingUser: to, status: .ended(reason: .error))
+                            callsStorage[badCall.id] = badCall
+                            currentCallsStorage[to.id] = nil
+                            currentCallsStorage[from.id] = nil
+                            return nil
+                        }
+                        let goodCall = Call(id: call.id, incomingUser: from, outgoingUser: to, status: .talk)
+                        callsStorage[goodCall.id] = goodCall
+                        currentCallsStorage[to.id] = goodCall
+                        currentCallsStorage[from.id] = goodCall
+                        return goodCall.id
+                    }
+                    
+                case .end(from: let from):
+                    guard let call = currentCall(user: from) else {
+                        return nil
+                    }
+                    let to = call.outgoingUser.id == from.id ? call.incomingUser : call.outgoingUser
+                    
+                    if !usersStorage.contains(from) || !usersStorage.contains(to){
+                        let badCall = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .ended(reason: .error))
+                        callsStorage[badCall.id] = badCall
+                        currentCallsStorage[from.id] = nil
+                        currentCallsStorage[to.id] = nil
+                        return nil
+                    }
+                    if (currentCall(user: to)?.status != .talk) {
+                        let badCall = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .ended(reason: .cancel))
+                        callsStorage[badCall.id] = badCall
+                        currentCallsStorage[from.id] = nil
+                        currentCallsStorage[to.id] = nil
+                        return badCall.id
+                    }
+                    let goodCall = Call(id: call.id, incomingUser: call.incomingUser, outgoingUser: call.outgoingUser, status: .ended(reason: .end))
+                    callsStorage[goodCall.id] = goodCall
+                    currentCallsStorage[from.id] = nil
+                    currentCallsStorage[to.id] = nil
+                    return goodCall.id
+                    
+                }
+                return nil
     }
     
     func calls() -> [Call] {
-        []
+        Array(callsStorage.values)
     }
     
     func calls(user: User) -> [Call] {
-        []
+        Array(callsStorage.values.filter { $0.incomingUser.id == user.id || $0.outgoingUser.id == user.id})
+            
     }
     
     func call(id: CallID) -> Call? {
-        nil
+        callsStorage[id]
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        currentCallsStorage[user.id]
     }
 }
